014be793888596ffe6499e9ac31eac57de0bf2a2
Pradin <56289715+inzeepadtip@users.noreply.github.com>
2021-12-02 12:26:58 +0700

    done easyTask3.4

--- src/app/core/task.service.ts
+++ src/app/core/task.service.ts
@@ -9,7 +9,7 @@ export class TaskService {
 
   private oddNumbers(range: number) {
     var n = 1;
-    const oddNumbers = [];
+    var oddNumbers = [];
     while (oddNumbers.length < range) {
       if (n % 2 != 0) {
         oddNumbers.push(n);
@@ -21,7 +21,7 @@ export class TaskService {
 
   private primeNumbers(range: number) {
     var n = 2;
-    const primeNumbers = [];
+    var primeNumbers = [];
     while (primeNumbers.length < range) {
       var primeNumber = true;
       for (var i = 2; i < n; i++) {
@@ -36,11 +36,11 @@ export class TaskService {
     return primeNumbers;
   }
 
-  public task1() {
-    const listRange100 = [...Array(100).keys()].map(x => x + 1);
-    const listAnswer = listRange100.map(x => {
-      const multiplesOfThree = x % 3 == 0;
-      const multiplesOfFive = x % 5 == 0;
+  public easyTask1() {
+    var listRange100 = [...Array(100).keys()].map(x => x + 1);
+    var listAnswer = listRange100.map(x => {
+      var multiplesOfThree = x % 3 == 0;
+      var multiplesOfFive = x % 5 == 0;
       if (multiplesOfThree && multiplesOfFive) {
         return "FizzBuzz";
       } else if (multiplesOfThree) {
@@ -54,18 +54,18 @@ export class TaskService {
     return listAnswer.join(" ");
   }
 
-  public task2(year: number) {
-    const year2Digit = year.toString().split('')
+  public easyTask2(year: number) {
+    var year2Digit = year.toString().split('')
     while (year2Digit.length > 2) {
       year2Digit.shift()
     }
     var year2Digitnumber = + year2Digit.join("");
-    const divisibleBy400 = year % 400 === 0;
-    const divisibleBy4 = year2Digitnumber % 4 === 0;
+    var divisibleBy400 = year % 400 === 0;
+    var divisibleBy4 = year2Digitnumber % 4 === 0;
     return divisibleBy400 || (year2Digitnumber > 0 && divisibleBy4);
   }
 
-  public task3Dot1(n: number) {
+  public easyTask3Dot1(n: number) {
     var str = "";
     for (var i = 0; i < n; i++) {
       str += `${'*'.repeat(i)}\n`;
@@ -73,7 +73,7 @@ export class TaskService {
     return str;
   }
 
-  public task3Dot2(n: number) {
+  public easyTask3Dot2(n: number) {
     var str = "";
     for (var i = 0; i < n; i++) {
       str += `${' '.repeat(n - i) + '*'.repeat(i)}\n`;
@@ -81,7 +81,7 @@ export class TaskService {
     return str;
   }
 
-  public task3Dot3(n: number) {
+  public easyTask3Dot3(n: number) {
     var str = "";
     var oddNumbers = this.oddNumbers(n - 1);
     for (var i = 0; i < n; i++) {
@@ -90,24 +90,34 @@ export class TaskService {
     return str;
   }
 
-  public task3Dot4(n: number) {
+  public easyTask3Dot4(n: number) {
     var str = "";
-    // var oddNumbers = this.oddNumbers(n / 2 - 1).reverse();
-    // console.log(oddNumbers)
-    // for (var i = 0; i < n; i++) {
-    //   str += `${' '.repeat(n - i) + '*' + ' '.repeat(oddNumbers[i - 1]) + (oddNumbers[i - 1] ? '*' : '')}\n`;
-    // }
+    var halfN = Math.trunc(n / 2);
+    var isEvenNumber = n % 2 == 0;
+    var oddNumbers = this.oddNumbers(n / 2).reverse();
+    oddNumbers.shift();
+    if(isEvenNumber){
+      oddNumbers = oddNumbers.map(x => x+=1);
+    }
+    for (var i = 0; i < n; i++) {
+      let blankQuantity = halfN - Math.abs(halfN - i);
+      (isEvenNumber && i >= halfN) && blankQuantity--;
+      let oddIndex = blankQuantity;
+      str += `${' '.repeat(blankQuantity) + '*' + ' '.repeat(oddNumbers[oddIndex]) + ((!isEvenNumber && i != halfN) || isEvenNumber ? '*' : '')}\n`;
+    }
     return str;
   }
 
-  public task3Dot5(n: number) {
+  public easyTask3Dot5(n: number) {
     var str = "";
+    var halfN = Math.trunc(n / 2);
+    var isEvenNumber = n % 2 == 0;
     var oddNumbers = this.oddNumbers(n / 2);
     for (var i = 0; i < n; i++) {
-      let blankQuantity = Math.abs((Math.trunc(n / 2) - i));
-      (n % 2 == 0 && i >= Math.trunc(n / 2)) && blankQuantity++;
-      let oddIndex = Math.trunc(n / 2) - blankQuantity;
-      str += `${' '.repeat(n % 2 == 0 ? blankQuantity - 1 : blankQuantity) + '*'.repeat(oddNumbers[oddIndex])}\n`;
+      let blankQuantity = Math.abs(halfN - i);
+      (isEvenNumber && i >= halfN) && blankQuantity++;
+      let oddIndex = halfN - blankQuantity;
+      str += `${' '.repeat(isEvenNumber ? blankQuantity - 1 : blankQuantity) + '*'.repeat(oddNumbers[oddIndex])}\n`;
     }
     return str;
   }
